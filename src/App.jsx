c462fef21abce3113e89a4a2679d9da0029100c1
Rodrigo Silva de Sena <100447199+Rodrigo1163@users.noreply.github.com>
2024-01-25 14:03:02 +0000

    duas imagem, bordas e centralização adicionadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+    <div className="container">
+      <div className='card'>
         <h2>Rick Sanchez</h2>
         <img src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'/>
       </div>
+      <div className='card'>
+        <h2>Rick Sanchez</h2>
+        <img src='https://rickandmortyapi.com/api/character/avatar/2.jpeg'/>
+      </div>
+    </div>
+      
     </>
   )
 }
